ba0c2631ffb1edf3833a70675e174b51c1b64c02
andrei <dam195@yahoo.com>
2023-03-27 18:38:00 +0300

    pb sectiune servicii

--- src/components/Home/Servicii/ServiciiSection.tsx
+++ src/components/Home/Servicii/ServiciiSection.tsx
@@ -4,7 +4,7 @@ import dateServicii from "./dateServicii";
 
 const ServiciiSection = () => {
   return (
-    <section className="container flex w-full flex-col items-center text-center lg:gap-8 lg:pt-24">
+    <section className="container flex w-full flex-col items-center text-center md:gap-8 md:py-24 md:pb-32">
       <Typography
         variant="h5"
         className="p-16 font-bold text-gri-brand md:text-3xl "
