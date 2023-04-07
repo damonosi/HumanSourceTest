4d0168b1fe578d3113656fdbfa40a606da89e691
andrei <dam195@yahoo.com>
2023-04-07 21:18:22 +0300

    text nevoi section

--- src/components/Home/Nevoi/NevoiSection.tsx
+++ src/components/Home/Nevoi/NevoiSection.tsx
@@ -7,19 +7,16 @@ const NevoiSection = () => {
     <section className="container relative flex w-full items-start justify-start bg-[#506673] px-2 pb-24 md:pr-6  md:pb-[0px]   ">
       <div className="flex w-full flex-col-reverse  md:flex-row ">
         <div
-          className="flex  max-w-[606px] flex-col justify-center gap-12   md:w-full md:pr-2 "
+          className="flex  max-w-[606px] flex-col justify-center gap-12   text-alb-site md:w-full md:pr-2"
           id="text"
         >
-          <Typography
-            variant="h3"
-            className="text-start font-bold text-alb-site  "
-          >
+          <Typography variant="h3" className="text-start font-bold   ">
             In ce situatii ai nevoie de noi?
           </Typography>
 
           <Typography
             variant="h6"
-            className=" text-start font-[500] text-alb-site "
+            className=" text-start font-[500]  md:text-xl"
           >
             &#x2713; &nbsp; Am o companie intr-o alta tara si am nevoie de
             specialisti romani, insa nu doresc sa imi deschid o filiala in
@@ -27,16 +24,16 @@ const NevoiSection = () => {
           </Typography>
 
           <Typography
-            variant="h5"
-            className="text-start  font-[500] text-alb-site "
+            variant="h6"
+            className="text-start  font-[500] md:text-xl "
           >
             &#x2713; &nbsp; Una dintre angajatele tale de baza intra in curand
             in concediu de maternitate. Cine ii va face treaba la fel de bine ?
           </Typography>
 
           <Typography
-            variant="h5"
-            className="text-start font-[500] text-alb-site "
+            variant="h6"
+            className="text-start  font-[500]  md:text-xl"
           >
             &#x2713; &nbsp; Cum pot angaja un numar mare de oameni, in perioada
             aceasta foarte aglomerata, daca nu am departament de HR?
