b84c891a09d88d75cd6ef11ca25fd76d9d4d9948
andrei <dam195@yahoo.com>
2023-05-01 13:37:31 +0300

    servicii butoane

--- src/app/[lang]/despre-noi/page.tsx
+++ src/app/[lang]/despre-noi/page.tsx
@@ -11,99 +11,67 @@ import { dateEchipa, dateValori } from "./dateCarduriAbout";
 
 const DespreNoiPage = () => {
   return (
-    <section className="bg-[#E5E5E5] px-5 pb-[100px] md:px-[70px] ">
-      <div className="container mx-auto flex flex-col ">
-        <Breadcrumbs
-        >
-          <Link className="text-gri-brand" href="/">
-            Home
-          </Link>
-          <Link className="text-red-600" href="/despre-noi">
-            Despre Noi
-          </Link>
-        </Breadcrumbs>
-        <div className="container mt-[30px] md:mt-14 flex w-full flex-col-reverse gap-6 pb-[100px]  lg:flex-row ">
-          <PageTransitionWrapper
-            className="grid w-full  items-start gap-6 text-left lg:w-1/2"
-            id="text-about"
-          >
-            <Typography
-              variant="h3"
-              className=" text-bold text-left text-gri-brand "
-            >
-              Despre noi
-            </Typography>
-            <Typography
-              variant="h5"
-              className="max-w-[33rem] text-left font-bold leading-[30px] text-gri-brand "
-            >
-              Companie de resurse umane ce oferă servicii de atât pe partea de
-              leasign de personal, recrutare, selecție cât și pe partea de
-              payroll si administrare de personal.
-            </Typography>
-            <Typography
-              variant="paragraph"
-              className="text-left font-normal text-gri-brand  opacity-80 "
-            >
-              Lorem ipsum dolor sit amet consectetur. Cras felis tristique
-              pharetra magna. Orci quis dui viverra mi lacus amet pulvinar quis.
-              Eu auctor ullamcorper imperdiet ultricies amet elementum quam.
-              Tellus libero massa laoreet consequat porta id orci tempus.
-              Dapibus quis curabitur vitae egestas sit feugiat tristique enim
-              id. Enim lacus enim donec eget elementum. Mi imperdiet viverra
-              tellus adipiscing amet vel risus. Nulla libero pulvinar nullam
-              posuere blandit dignissim viverra eu magna. Blandit euismod risus
-              vel leo.
-            </Typography>
-          </PageTransitionWrapper>{" "}
-          <div
-            className="flex w-full items-center justify-center md:w-1/2"
-            id="imagine-about"
-          >
-            {" "}
-            <Image alt="imagine-about" className="z-20 w-full" src={Hi5} />
-          </div>
-        </div>
-        <section className="flex flex-col pb-[100px]  text-center md:px-0">
-          <Typography variant="h3" className="bold pb-[60px]">
-            Valorile Companiei
-          </Typography>
-          <div
-            className="container mx-auto grid grid-cols-1 flex-col   gap-5 sm:grid-cols-2 lg:grid-cols-4 "
-            id="container-carduri-about"
-          >
-            {dateValori.map(({ titlu, descriere, src, id }) => (
-              <CardValori
-                key={id}
-                titlu={titlu}
-                descriere={descriere}
-                src={src}
-              />
-            ))}
-          </div>
-        </section>
-        <section
-          className="flex w-full flex-col items-center   "
-          id="container echipa"
-        >
-          <div className=" text-center">
-            <Typography
-              variant="h3"
-              className="pb-[60px] font-bold  text-gri-brand"
-            >
-              Echipa noastra
-            </Typography>
-          </div>
+		<section className="bg-[#E5E5E5] px-5 pb-[100px] md:px-[70px] ">
+			<div className="container mx-auto flex flex-col">
+				<Breadcrumbs>
+					<Link className="text-gri-brand" href="/">
+						Home
+					</Link>
+					<Link className="text-red-600" href="/despre-noi">
+						Despre Noi
+					</Link>
+				</Breadcrumbs>
+				<div className="container mt-[30px] flex w-full flex-col-reverse gap-6 pb-[100px] md:mt-14  lg:flex-row ">
+					<PageTransitionWrapper className="grid w-full  items-start gap-6 text-left lg:w-1/2" id="text-about">
+						<Typography variant="h3" className=" text-bold text-left text-gri-brand ">
+							Despre noi
+						</Typography>
+						<Typography variant="h5" className="max-w-[33rem] text-left font-bold leading-[30px] text-gri-brand ">
+							Companie de resurse umane ce oferă servicii de atât pe partea de leasign de personal, recrutare, selecție
+							cât și pe partea de payroll si administrare de personal.
+						</Typography>
+						<Typography variant="paragraph" className="text-left font-normal text-gri-brand  opacity-80 ">
+							Lorem ipsum dolor sit amet consectetur. Cras felis tristique pharetra magna. Orci quis dui viverra mi
+							lacus amet pulvinar quis. Eu auctor ullamcorper imperdiet ultricies amet elementum quam. Tellus libero
+							massa laoreet consequat porta id orci tempus. Dapibus quis curabitur vitae egestas sit feugiat tristique
+							enim id. Enim lacus enim donec eget elementum. Mi imperdiet viverra tellus adipiscing amet vel risus.
+							Nulla libero pulvinar nullam posuere blandit dignissim viverra eu magna. Blandit euismod risus vel leo.
+						</Typography>
+					</PageTransitionWrapper>{" "}
+					<div className="flex w-full items-center justify-center md:w-1/2" id="imagine-about">
+						{" "}
+						<Image alt="imagine-about" className="z-20 w-full" src={Hi5} />
+					</div>
+				</div>
+				<section className="flex flex-col pb-[100px]  text-center md:px-0">
+					<Typography variant="h3" className="bold pb-[60px]">
+						Valorile Companiei
+					</Typography>
+					<div
+						className="container mx-auto grid grid-cols-1 flex-col   gap-5 sm:grid-cols-2 lg:grid-cols-4 "
+						id="container-carduri-about"
+					>
+						{dateValori.map(({ titlu, descriere, src, id }) => (
+							<CardValori key={id} titlu={titlu} descriere={descriere} src={src} />
+						))}
+					</div>
+				</section>
+				<section className="flex w-full flex-col items-center   " id="container echipa">
+					<div className=" text-center">
+						<Typography variant="h3" className="pb-[60px] font-bold  text-gri-brand">
+							Echipa noastra
+						</Typography>
+					</div>
 
-          <div className="grid w-full gap-8 md:grid-cols-2 md:px-8 lg:grid-cols-3 ">
-            {dateEchipa.map(({ nume, pozitie, src, id }) => (
-              <CardEchipa key={id} nume={nume} pozitie={pozitie} src={src} />
-            ))}
-          </div>
-        </section>
-      </div>
-    </section>
-  );
+					<div className="grid w-full gap-8 md:grid-cols-2 md:px-8 lg:grid-cols-3 ">
+						{dateEchipa.map(({ nume, pozitie, src, id }) => (
+							<CardEchipa key={id} nume={nume} pozitie={pozitie} src={src} />
+						))}
+					</div>
+				</section>
+			</div>
+		</section>
+	);
 };
 
 export default DespreNoiPage;
--- src/components/Blog/CardBlogSecundar.tsx
+++ src/components/Blog/CardBlogSecundar.tsx
@@ -20,32 +20,26 @@ const CardBlogSecundar = ({
   params
 }: ICardBlogSecundar) => {
   return (
-    <Link className="cursor-pointer hover:shadow-xl py-2 px-4" href={`${params.lang}/blog/${slug}`}>
-      <div className="flex flex-col gap-5" id="container-card-blog-secundar">
-        <div className="flex flex-col">
-          <Image alt="imagine-blog" src={src} />
-        </div>
-        <div className="flex flex-col gap-2">
-          {" "}
-          <Typography
-            variant="small"
-            className="text-start text-gri-brand opacity-50"
-          >
-            {data}
-          </Typography>
-          <Typography
-            variant="h4"
-            className="text-start font-bold text-gri-brand"
-          >
-            {descriere}
-          </Typography>
-          <Typography variant="paragraph" className="text-start text-red-600">
-            {categorie}
-          </Typography>
-        </div>
-      </div>
-    </Link>
-  );
+		<Link className="cursor-pointer rounded-2xl hover:shadow-xl " href={`${params.lang}/blog/${slug}`}>
+			<div className="flex flex-col gap-5" id="container-card-blog-secundar">
+				<div className="flex w-full flex-col overflow-hidden rounded-t-2xl">
+					<Image alt="imagine-blog" src={src} />
+				</div>
+				<div className="flex flex-col gap-2 px-2 pb-4">
+					{" "}
+					<Typography variant="small" className="text-start text-gri-brand opacity-50">
+						{data}
+					</Typography>
+					<Typography variant="h4" className="text-start font-bold text-gri-brand">
+						{descriere}
+					</Typography>
+					<Typography variant="paragraph" className="text-start text-red-600">
+						{categorie}
+					</Typography>
+				</div>
+			</div>
+		</Link>
+	);
 };
 
 export default CardBlogSecundar;
