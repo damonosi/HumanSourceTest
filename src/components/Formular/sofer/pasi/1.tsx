7d416e3b75e239d5209e1306f64968953c1e1b43
andrei <dam195@yahoo.com>
2023-05-02 16:29:38 +0300

    errors

--- src/components/Formular/sofer/pasi/1.tsx
+++ src/components/Formular/sofer/pasi/1.tsx
@@ -11,47 +11,47 @@ const Pas1Trasport = (setValue: any) => {
 		<FormWrapper intrebare="Cu ce tip de remorca va doriti sa lucrati?">
 			<div className="grid grid-cols-3 place-items-center gap-8">
 				<CardRemorca
-					onClick={setValue("tipRemorca", "cisterna")}
+					onClick={() => setValue("tipRemorca", "cisterna")}
 					text="Cisterna"
 					svg={<Cisterna2 className="h-36 w-56 " />}
 				/>
 				<CardRemorca
-					onClick={setValue("tipRemorca", "frigorifica")}
+					onClick={() => setValue("tipRemorca", "frigorifica")}
 					text="Frigorifica"
 					svg={<Cisterna2 className="h-36 w-56 " />}
 				/>
 				<CardRemorca
-					onClick={setValue("tipRemorca", "prelata")}
+					onClick={() => setValue("tipRemorca", "prelata")}
 					text="Prelata"
 					svg={<Cisterna2 className="h-36 w-56 " />}
 				/>
 				<CardRemorca
-					onClick={setValue("tipRemorca", "basculanta")}
+					onClick={() => setValue("tipRemorca", "basculanta")}
 					text="Basculanta"
 					svg={<Cisterna2 className="h-36 w-56 " />}
 				/>
 				<CardRemorca
-					onClick={setValue("tipRemorca", "animale")}
+					onClick={() => setValue("tipRemorca", "animale")}
 					text="Box"
 					svg={<Cisterna2 className="h-36 w-56 " />}
 				/>
 				<CardRemorca
-					onClick={setValue("tipRemorca", "cisterna")}
+					onClick={() => setValue("tipRemorca", "cisterna")}
 					text="Animale"
 					svg={<Cisterna2 className="h-36 w-56 " />}
 				/>
 				<CardRemorca
-					onClick={setValue("tipRemorca", "siloz")}
+					onClick={() => setValue("tipRemorca", "siloz")}
 					text="Siloz"
 					svg={<Cisterna2 className="h-36 w-56 " />}
 				/>
 				<CardRemorca
-					onClick={setValue("tipRemorca", "container")}
+					onClick={() => setValue("tipRemorca", "container")}
 					text="Container"
 					svg={<Cisterna2 className="h-36 w-56 " />}
 				/>
 				<CardRemorca
-					onClick={setValue("tipRemorca", "agabaritic")}
+					onClick={() => setValue("tipRemorca", "agabaritic")}
 					text="Agabaritic"
 					svg={<Cisterna2 className="h-36 w-56 " />}
 				/>
