ccb151b5f0dd3af65b831875cd0dc010eb2b0e7f
andrei <dam195@yahoo.com>
2023-05-02 19:17:49 +0300

    camioane inceput animatie

--- src/components/Formular/sofer/CardRemorca.tsx
+++ src/components/Formular/sofer/CardRemorca.tsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
+
 interface ICardProps {
 	text: string;
+	className?: string;
 	svg: JSX.Element;
 	onClick: () => void;
 }
 
-const CardRemorca = ({ svg, text, onClick }: ICardProps) => {
+const CardRemorca = ({ svg, text, onClick, className }: ICardProps) => {
 	return (
 		<button
 			onClick={onClick}
 			type="button"
-			className="flex max-w-[272px] flex-col items-center justify-center rounded-2xl bg-alb-site py-16 px-5"
+			className={`flex max-w-[272px] flex-col items-center justify-center rounded-2xl  bg-alb-site py-16 px-5 drop-shadow-xl active:bg-gri-brand ${className} `}
 		>
 			{svg}
 			<span>{text}</span>
--- src/components/Formular/sofer/pasi/1.tsx
+++ src/components/Formular/sofer/pasi/1.tsx
@@ -2,56 +2,103 @@
 import FormWrapper from "../../FormWrapper";
 import { useState } from "react";
 import CardRemorca from "../CardRemorca";
-import Cisterna2 from "../../../../public/imagini/formular/sofer/prelata.svg";
+import Cisterna from "../../../../public/imagini/formular/sofer/negru/cisterna.svg";
+import Frigorifica from "../../../../public/imagini/formular/sofer/negru/frigorifica.svg";
+import FrigorificaAlb from "../../../../public/imagini/formular/sofer/alb/frigorifica.svg";
+import Prelata from "../../../../public/imagini/formular/sofer/negru/prelata.svg";
+import Basculanta from "../../../../public/imagini/formular/sofer/negru/basculanta.svg";
+import Box from "../../../../public/imagini/formular/sofer/negru/box.svg";
+import Animale from "../../../../public/imagini/formular/sofer/negru/animale.svg";
+import Siloz from "../../../../public/imagini/formular/sofer/negru/siloz.svg";
+import Container from "../../../../public/imagini/formular/sofer/negru/container.svg";
+import Agabaritic from "../../../../public/imagini/formular/sofer/negru/agabaritic.svg";
 
 const Pas1Trasport = ({ setValue }: any) => {
+	const [selected, setSelected] = useState(0);
+	console.log(selected);
 	return (
 		<FormWrapper intrebare="Cu ce tip de remorca va doriti sa lucrati?">
 			<div className="grid grid-cols-3 place-items-center gap-8">
 				<CardRemorca
-					onClick={() => setValue("tipRemorca", "cisterna")}
+					className={`${selected == 1 && "bg-gri-brand"}`}
+					onClick={() => {
+						setValue("tipRemorca", "cisterna");
+						setSelected(1);
+					}}
 					text="Cisterna"
-					svg={<Cisterna2 className="h-36 w-56 " />}
+					svg={<Cisterna className="h-36 w-56 " />}
 				/>
 				<CardRemorca
-					onClick={() => setValue("tipRemorca", "frigorifica")}
+					className={`${selected == 2 && "bg-gri-brand"}`}
+					onClick={() => {
+						setValue("tipRemorca", "frigorifica");
+						setSelected(2);
+					}}
 					text="Frigorifica"
-					svg={<Cisterna2 className="h-36 w-56 " />}
+					svg={selected == 2 ? <FrigorificaAlb className="h-36 w-56 " /> : <Frigorifica className="h-36 w-56 " />}
 				/>
 				<CardRemorca
-					onClick={() => setValue("tipRemorca", "prelata")}
+					className={`${selected == 3 && "bg-gri-brand"}`}
+					onClick={() => {
+						setValue("tipRemorca", "prelata");
+						setSelected(3);
+					}}
 					text="Prelata"
-					svg={<Cisterna2 className="h-36 w-56 " />}
+					svg={<Prelata className="h-36 w-56 " />}
 				/>
 				<CardRemorca
-					onClick={() => setValue("tipRemorca", "basculanta")}
+					className={`${selected == 4 && "bg-gri-brand"}`}
+					onClick={() => {
+						setValue("tipRemorca", "basculanta");
+						setSelected(4);
+					}}
 					text="Basculanta"
-					svg={<Cisterna2 className="h-36 w-56 " />}
+					svg={<Basculanta className="h-36 w-56 " />}
 				/>
 				<CardRemorca
-					onClick={() => setValue("tipRemorca", "animale")}
+					className={`${selected == 5 && "bg-gri-brand"}`}
+					onClick={() => {
+						setValue("tipRemorca", "animale");
+						setSelected(5);
+					}}
 					text="Box"
-					svg={<Cisterna2 className="h-36 w-56 " />}
+					svg={<Box className="h-36 w-56 " />}
 				/>
 				<CardRemorca
-					onClick={() => setValue("tipRemorca", "cisterna")}
+					className={`${selected == 6 && "bg-gri-brand"}`}
+					onClick={() => {
+						setValue("tipRemorca", "cisterna");
+						setSelected(6);
+					}}
 					text="Animale"
-					svg={<Cisterna2 className="h-36 w-56 " />}
+					svg={<Animale className="h-36 w-56 " />}
 				/>
 				<CardRemorca
-					onClick={() => setValue("tipRemorca", "siloz")}
+					className={`${selected == 7 && "bg-gri-brand"}`}
+					onClick={() => {
+						setValue("tipRemorca", "siloz");
+						setSelected(7);
+					}}
 					text="Siloz"
-					svg={<Cisterna2 className="h-36 w-56 " />}
+					svg={<Siloz className="h-36 w-56 " />}
 				/>
 				<CardRemorca
-					onClick={() => setValue("tipRemorca", "container")}
+					className={`${selected == 8 && "bg-gri-brand"}`}
+					onClick={() => {
+						setValue("tipRemorca", "container");
+						setSelected(8);
+					}}
 					text="Container"
-					svg={<Cisterna2 className="h-36 w-56 " />}
+					svg={<Container className="h-36 w-56 " />}
 				/>
 				<CardRemorca
-					onClick={() => setValue("tipRemorca", "agabaritic")}
+					className={`${selected == 9 && "bg-gri-brand"}`}
+					onClick={() => {
+						setValue("tipRemorca", "agabaritic");
+						setSelected(9);
+					}}
 					text="Agabaritic"
-					svg={<Cisterna2 className="h-36 w-56 " />}
+					svg={selected == 9 ? <Agabaritic className="h-36 w-56   " /> : <Agabaritic className="h-36 w-56  " />}
 				/>
 			</div>
 		</FormWrapper>
