929544bbc739ed44f16f5757920538b5f6d67ac1
andrei <dam195@yahoo.com>
2023-05-05 17:13:05 +0300

    hero subtitlu size

--- src/components/Home/Hero/HeroSection.tsx
+++ src/components/Home/Hero/HeroSection.tsx
@@ -17,54 +17,45 @@ const HeroSection = (
   const { t } = useTranslation(params.lang, 'home-hero');
 
   return (
-    <section
-      id="hero-section"
-      className=" relative mx-0 flex flex-col   items-center overflow-hidden  bg-gri-bg  pt-14  md:flex-row    md:justify-center    "
-    >
-      <div className=" container  flex w-full  flex-col-reverse   items-center gap-12  bg-gradient-to-b  pb-56 lg:mx-16 lg:flex-row-reverse  ">
-        <div className=" relative hidden h-full md:block md:w-1/2">
-          <HartaHeroDesktop />
-        </div>
-        <div
-          className="  flex w-full flex-col md:hidden"
-          id="container harta carduri mobil"
-        >
-          <div className="  flex  w-full items-center py-4">
-            <HartaHeroMobil />
-          </div>
+		<section
+			id="hero-section"
+			className=" relative mx-0 flex flex-col   items-center overflow-hidden  bg-gri-bg  pt-14  md:flex-row    md:justify-center    "
+		>
+			<div className=" container  flex w-full  flex-col-reverse   items-center gap-12  bg-gradient-to-b  pb-56 lg:mx-16 lg:flex-row-reverse  ">
+				<div className=" relative hidden h-full md:block md:w-1/2">
+					<HartaHeroDesktop />
+				</div>
+				<div className="  flex w-full flex-col md:hidden" id="container harta carduri mobil">
+					<div className="  flex  w-full items-center py-4">
+						<HartaHeroMobil />
+					</div>
 
-          <CarduriHero />
-        </div>
-        <div
-          className="  z-30 flex flex-col-reverse items-center justify-start gap-16 px-6   text-alb-site md:flex-col md:gap-0 md:px-0 lg:w-2/4    "
-          id="container-text-carduri"
-        >
-          <div
-            className=" flex  w-full flex-col gap-6 pt-6 text-start text-alb-site  lg:pb-16"
-            id="container-text"
-          >
-            <Typography variant="small" className="uppercase opacity-80">
-              {/* // eslint-disable-next-line @typescript-eslint/ban-ts-comment 
+					<CarduriHero />
+				</div>
+				<div
+					className="  z-30 flex flex-col-reverse items-center justify-start gap-16 px-6   text-alb-site md:flex-col md:gap-0 md:px-0 lg:w-2/4    "
+					id="container-text-carduri"
+				>
+					<div className=" flex  w-full flex-col gap-6 pt-6 text-start text-alb-site  lg:pb-16" id="container-text">
+						<Typography variant="small" className="uppercase opacity-80">
+							{/* // eslint-disable-next-line @typescript-eslint/ban-ts-comment 
               // @ts-ignore */}
-              {t('titlu')}
-            </Typography>
-            <Typography
-              variant="h1"
-              className="text-start font-bold text-alb-site  "
-            >
-              {t('subtitlu')}
-            </Typography>
-            <Typography variant="paragraph" className="font-[350]  opacity-80">
-              {t('indemn')}
-            </Typography>
-          </div>
-          <div className=" hidden w-full md:block">
-            <CarduriHero />
-          </div>
-        </div>{" "}
-      </div>
-    </section>
-  );
+							{t("titlu")}
+						</Typography>
+						<Typography variant="h1" className="text-start text-3xl font-bold text-alb-site md:text-5xl  ">
+							{t("subtitlu")}
+						</Typography>
+						<Typography variant="paragraph" className="font-[350]  opacity-80">
+							{t("indemn")}
+						</Typography>
+					</div>
+					<div className=" hidden w-full md:block">
+						<CarduriHero />
+					</div>
+				</div>{" "}
+			</div>
+		</section>
+	);
 };
 
 export default HeroSection;
