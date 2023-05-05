8fd1c115133f6639c47813fe6e91a26c7affbe0d
andrei <dam195@yahoo.com>
2023-05-05 17:42:26 +0300

    trimite email si suna whapp butoane contact cu tooltip

--- src/app/[lang]/contact/page.tsx
+++ src/app/[lang]/contact/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Typography } from "@material-tailwind/react";
+import { Tooltip, Typography } from "@material-tailwind/react";
 import EmailIcon from "@mui/icons-material/Email";
 
 import PhoneIcon from "@mui/icons-material/Phone";
@@ -38,12 +38,34 @@ const Contact = ({ params }: { params: { lang: string; country: string } }) => {
 							</p>
 						</div>
 						<div className="flex w-full flex-col items-start gap-2 md:flex-row">
-							<div className="rounded-full border bg-white py-2 px-3">
-								<EmailIcon /> <span className="ml-3"> office@humansource.com</span>
-							</div>
-							<div className="rounded-full border bg-white py-2 px-3">
-								<PhoneIcon /> <span className="ml-3"> +40209764893</span>
-							</div>
+							<Tooltip
+								content="Trimiteti un email"
+								placement="bottom"
+								animate={{
+									mount: { scale: 1, y: 5 },
+									unmount: { scale: 0, y: 0 },
+								}}
+							>
+								<Link href="mailto:office@humansource.com">
+									<div className="rounded-full border bg-white py-2 px-3">
+										<EmailIcon /> <span className="ml-3"> office@humansource.com</span>
+									</div>
+								</Link>
+							</Tooltip>
+							<Tooltip
+								content="Sunati pe whatts up"
+								placement="bottom"
+								animate={{
+									mount: { scale: 1, y: 5 },
+									unmount: { scale: 0, y: 0 },
+								}}
+							>
+								<Link href="http://api.whatsapp.com/send?phone=0209764893">
+									<div className="rounded-full border bg-white py-2 px-3">
+										<PhoneIcon /> <span className="ml-3"> +40209764893</span>
+									</div>
+								</Link>
+							</Tooltip>
 						</div>
 						<ContactForm />
 					</div>
